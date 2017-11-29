c69683a23f1c4722c8b8993d53039657f0a2e0a8
Silvia-C <33365850+Silvia-C@users.noreply.github.com>
2017-11-29 05:07:41 -0500

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -6,6 +6,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y for yes, N for no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
